22d6636d8697123fc807af6b4e0c4749e684cbdf
Xiaoming Zhang <2585537Z@gla.student.ac.uk>
2021-08-05 22:49:42 +0100

    search3

--- static/js/search.js
+++ static/js/search.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-    $("#index_search").click(function(){
-        var search_item = $("#search").val().trim()
-        $.ajax({
-            url:'search/search/',
-            type:'POST',
-            data:{search_item:search_item},
-            success:function(data){
-                alert(data)
-            },
-            fail:function(data){
-                alert("fail")
-            }
-        })
-    })
-})
+// $(document).ready(function(){
+//     $("#index_search").click(function(){
+//         var search_item = $("#search").val().trim()
+//         $.ajax({
+//             url:'search/search/',
+//             type:'POST',
+//             data:{search_item:search_item},
+//             success:function(data){
+//                 window.location.href("")
+//             },
+//             fail:function(data){
+//                 alert("fail")
+//             }
+//         })
+//     })
+// })
