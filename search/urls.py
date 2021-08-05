9f732e36484cdf8a7013bd3fd635713fa4193192
Xiaoming Zhang <2585537Z@gla.student.ac.uk>
2021-08-05 22:49:42 +0100

    search3

--- search/urls.py
+++ search/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'search'
 
 urlpatterns = [
-    path('search/', views.search, name="search"),
+    # path('search/', views.search, name="search"),
+    path('search_do/', views.search_do, name="search"),
 ]
--- search/views.py
+++ search/views.py
@@ -4,10 +4,18 @@ from django.views.decorators.csrf import csrf_exempt
 from rango.models import Page
 
 
-@csrf_exempt
-def search(request):
-    if request.POST:
-        pages = Page.objects.all()
-        if pages==None:
-            return HttpResponse("null")
-        return HttpResponse(pages)
+def search_do(request):
+    search_item = request.GET.get('search')
+    pages = Page.objects.filter(title__icontains=search_item)
+    context = {}
+    context['current_page'] = pages
+    context['search_item'] = search_item
+    return render(request, "rango/search.html", context=context)
+
+# @csrf_exempt
+# def search(request):
+#     if request.POST:
+#         pages = Page.objects.all()
+#         if pages==None:
+#             return HttpResponse("null")
+#         return HttpResponse(pages)
